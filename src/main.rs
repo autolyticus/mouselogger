8bd9129e62c02831323b4aabc806526e76adc257
reisub0 <reisub0@gmail.com>
2019-02-14 18:26:27 +0530

    Adds mouselogger
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+//
+// main.rs
+// Copyright (C) 2019 g <g@ABCL>
+// Distributed under terms of the MIT license.
+//
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct input_event {
+    pub _click: u8,
+    pub x: i8,
+    pub y: i8,
+}
+
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    let p = Command::new("sh")
+        .arg("-c")
+        .arg("xdotool getmouselocation --shell | head -n2 | egrep -o [0-9]*")
+        .output()?;
+    let s: Vec<_> = String::from_utf8(p.stdout)
+        .unwrap()
+        .split('\n')
+        .take(2)
+        .map(|x| str::parse::<i32>(x).unwrap())
+        .collect();
+    let mut x = s[0];
+    let mut y = s[1];
+    let mut f = File::open("/dev/input/mice")?;
+    let mut buf = [0; std::mem::size_of::<input_event>()];
+    let mut ie: input_event;
+    loop {
+        f.read(&mut buf)?;
+        unsafe {
+            ie = std::mem::transmute(buf);
+        }
+        x += ie.x as i32;
+        y += ie.y as i32;
+        println!("x = {}, dx = {}, y = {}, dy = {}", x, ie.x, y, ie.y);
+    }
+    Ok(())
 }
